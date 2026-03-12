c86e681ce94f0bfd554e62d47931628a3b76682b
Harshi2005 <127752149+Harshi2005@users.noreply.github.com>
2026-03-12 23:01:03 +0530

    Update IRoom.java

--- src/Model/IRoom.java
+++ src/Model/IRoom.java
@@ -1,5 +1,10 @@
+// IRoom.java
 package model;
 
+/**
+ * Contract for any room entity in the hotel booking system.
+ * Exposes essential room properties and availability check.
+ */
 public interface IRoom {
     String getRoomNumber();
     Double getRoomPrice();
