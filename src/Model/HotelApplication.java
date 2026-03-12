647e3ce9619e5937140870c21de8ebb99bf9b89d
Harshi2005 <127752149+Harshi2005@users.noreply.github.com>
2026-03-12 23:19:14 +0530

    Update HotelApplication.java

--- src/Model/HotelApplication.java
+++ src/Model/HotelApplication.java
@@ -7,6 +7,6 @@ import com.sun.tools.javac.Main;
  */
 public class HotelApplication {
     public static void main(String[] args) {
-        MainMenu.mainMenu();
+        MainMenu.main(new String[0]);
     }
 }
