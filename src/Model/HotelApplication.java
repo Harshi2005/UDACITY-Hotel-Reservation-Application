dfd32c7d9b225d68c751219a6978590ac4921b05
Harshi2005 <127752149+Harshi2005@users.noreply.github.com>
2026-03-12 23:03:48 +0530

    Update HotelApplication.java

--- src/Model/HotelApplication.java
+++ src/Model/HotelApplication.java
@@ -1,5 +1,10 @@
+// HotelApplication.java
 import com.sun.tools.javac.Main;
 
+/**
+ * Application launcher that starts the entire hotel reservation program
+ * by invoking the main user menu system.
+ */
 public class HotelApplication {
     public static void main(String[] args) {
         MainMenu.mainMenu();
